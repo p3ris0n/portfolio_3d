72eb35a2ef953b01fe3b5f7f978cbc755190fcb9
Promise Raji <isaacsamson88@gmail.com>
2025-12-04 08:24:06 +0100

    added animation tp the rendered cube.

--- basic/main.js
+++ basic/main.js
@@ -1,16 +1,16 @@
 import * as THREE from 'three';
 
-// crreting the scene.
-const scene = THREE.scene = new THREE.Scene();
-scene.backgrouund = new THREE.Color('#F0F0F0');
+// creating the scene.
+const scene = new THREE.Scene();
+scene.background = new THREE.Color('#F0F0F0');
 
 // creating the camera.
-const camera = THREE.PerspectiveCamera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 5;
 
 // creating the object.
 const geometry  = new THREE.BoxGeometry();
-const material  = new THREE.MeshLambertMaterial({ color: '#468585', emissive: '#468585' })
+const material  = new THREE.MeshLambertMaterial({ color: '#468585', emissive: '#468585' });
 
 const cube = new THREE.Mesh(geometry, material);
 
@@ -19,13 +19,18 @@ scene.add(cube);
 
 // creating lighting.
 const light = new THREE.DirectionalLight(0x9CDBA6, 10);
-light.position.set(0, 1, 1);
+light.position.set(1, 1, 1);
 scene.add(light);
 
 // add renderer.
-const renderer = THREE.WebGLRenderer();
-renderer.setSize(window, innerWidth, window.innerHeight);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 // animation function for the scene.
-renderer.render(scene, camera);
+function animate() {
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render(scene, camera);
+}
+renderer.setAnimationLoop( animate );
